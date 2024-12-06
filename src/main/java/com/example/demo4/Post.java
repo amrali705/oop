c70c92d36a2ca074fb511b359b37b504f6253b04
unknown <omarsayedsaid990@gmail.com>
2024-12-06 05:42:50 +0200

    post omar

--- src/main/java/com/example/demo4/Post.java
+++ src/main/java/com/example/demo4/Post.java
@@ -1,4 +1,77 @@
 package com.example.demo4;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Post {
+    private String privacy;
+    private String content;
+    private List<Comment> comments;
+    private List<User> likes;
+    private User author;
+    private int post_id;
+    private Date time_post;
+
+    public Post(int post_id, String content, User author, Date time) {
+        this.post_id = post_id;
+        this.content = content;
+        this.author = author;
+        this.time_post = time;
+        this.likes = new ArrayList<>();
+        this.comments = new ArrayList<>();
+
+    }
+    public int getPostId() {
+        return post_id;
+    }
+    public void setPostId(int postId) {
+        this.post_id = postId;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public User getAuthor() {
+        return author;
+    }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+    public Date getTimestamp() {
+        return time_post;
+    }
+    public void setTimestamp(Date timestamp) {
+        this.time_post = timestamp;
+    }
+    public List<User> getLikes() {
+        return likes;
+    }
+    public void setLikes(List<User> likes) {
+        this.likes = likes;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    // Methods
+
+    public void addLike(User user) {
+        this.likes.add(user);
+    }
+    public void removeLike(User user) {
+        this.likes.remove(user);
+    }
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
+    public void editPost(String newContent) {
+        this.content = newContent;
+    }
+    public void deletePost() {
+    }
+
 }
