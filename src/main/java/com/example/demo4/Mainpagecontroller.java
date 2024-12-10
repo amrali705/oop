9e64a16f8cfa2d2d30d633493bc64425dd7d47c8
Amr Elhadad <amraliali@gmail.com>
2024-12-11 01:38:05 +0200

    login and account

--- src/main/java/com/example/demo4/Mainpagecontroller.java
+++ src/main/java/com/example/demo4/Mainpagecontroller.java
@@ -1,4 +1,24 @@
 package com.example.demo4;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class Mainpagecontroller {
+
+        @FXML
+        private TextField searchbar;
+
+        @FXML
+        private VBox vbox_of_friends;
+
+        @FXML
+        private VBox vbox_of_postes;
+
+        @FXML
+        void create_post(MouseEvent event) {
+
+        }
+
 }
+
