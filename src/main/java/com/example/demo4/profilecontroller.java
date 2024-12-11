50755409fd9aac7c9c20dd51d6e16d359e756692
Amr Elhadad <amraliali@gmail.com>
2024-12-11 22:22:06 +0200

    sd

--- src/main/java/com/example/demo4/profilecontroller.java
+++ src/main/java/com/example/demo4/profilecontroller.java
@@ -1,23 +1,27 @@
 package com.example.demo4;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
 public class profilecontroller {
+
+    @FXML
+    private Button home;
+
     @FXML
     private TextField searchbar;
 
     @FXML
-    private VBox vbox_of_friends;
+    private VBox vboxprofilepost;
 
     @FXML
-    private VBox vbox_of_postes;
-
-    @FXML
-    void create_post(MouseEvent event) {
-
+    void gomain(MouseEvent event) throws IOException {
+        new Mainpageview().mainpageview();
     }
 
 }
