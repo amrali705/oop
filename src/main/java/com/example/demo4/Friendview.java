6c5bac65846023a7f4d56fdc1be3597c84a664c8
mostafa <nasefmostafa05@gmail.com>
2024-12-13 05:14:44 +0200

    mostafa

---
+++ src/main/java/com/example/demo4/Friendview.java
@@ -0,0 +1,19 @@
+
+
+package com.example.demo4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Friendview {
+
+    @FXML
+    private Button openchat;
+
+    @FXML
+    void openchat(MouseEvent event) {
+
+    }
+
+}
