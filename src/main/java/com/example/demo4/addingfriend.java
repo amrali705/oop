b2d5c23aa93b9ca730d1bb92cdf55b8dcafbdcab
amral <amrali434@gmail.com>
2024-12-13 19:04:54 +0200

    amr

---
+++ src/main/java/com/example/demo4/addingfriend.java
@@ -0,0 +1,19 @@
+package com.example.demo4;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class addingfriend {
+
+    @FXML
+    private Label username;
+
+    @FXML
+    void addfriend(MouseEvent event) {
+
+
+    }
+
+}
